337fd13a54c9bc8ce8953fae6eae9461caf015dd
IKBAL AHMAD <116277826+ikbal9956@users.noreply.github.com>
2024-05-28 09:23:06 +0530

    Delete route directory

--- route/router.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { sendRev } from "../controller/userController.js";
-import { takes } from "../controller/usertaker.js";
-const router = express.Router();
-
-router.post("/send", sendRev);
-router.get("/take",takes);
-
-export default router;
